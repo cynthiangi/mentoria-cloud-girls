b774aba8445ef54209daa120f3dbb9507d0afe8e
Cynthia Angi <cynthia.ffa@gmail.com>
2024-05-06 17:59:59 -0300

    Criação pasta AluraPlay

---
+++ aluraplay/js/conecta-api.js
@@ -0,0 +1,30 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        }) //isso transforma o que estamos pedindo em string
+    });
+
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+export const conectaApi = { //exportando o que estamos colocando dentro da constante, no caso a função async para as funçoes listadas
+    listaVideos,
+    criaVideo
+}
